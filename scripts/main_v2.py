2b3c0d4fbc45b45cb3b1558bb17913f7e651dbf3
lucas8864 <145332098+lucas8864@users.noreply.github.com>
2026-09-12 16:52:38 +0800

    Update main_v2.py

--- scripts/main_v2.py
+++ scripts/main_v2.py
@@ -66,6 +66,7 @@ SOURCE_URLS = [
     "https://node.freeclashnode.com/uploads/2026/09/0-20260912.txt",
     "https://raw.githubusercontent.com/WLget/V2Ray_configs_64/refs/heads/master/ConfigSub_list.txt",
     "https://raw.githubusercontent.com/Au1rxx/free-vpn-subscriptions/main/output/v2ray-base64.txt",
+    "https://raw.githubusercontent.com/kooker/FreeSubsCheck/refs/heads/main/base64.txt",
 ]
 
 OUTPUT_DIR = "output"
